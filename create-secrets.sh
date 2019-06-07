5dd484f24a0a288b8bcc43ae997f97e20be25aef
Matt Elgin <melgin@cloudbees.com>
2019-06-07 16:06:14 -0400

    add kubectl command for kaniko secret

--- create-secrets.sh
+++ create-secrets.sh
@@ -3,3 +3,8 @@ kubectl create secret generic github --from-literal=username=${USERNAME} --from-
 kubectl create secret generic url --from-literal=url=${URL}
 
 kubectl create secret generic github-oauth --from-literal=CLIENT_ID=${CLIENT_ID} --from-literal=CLIENT_SECRET=${CLIENT_SECRET}
+
+kubectl create secret docker-registry docker-credentials \
+    --docker-username=${username}  \
+    --docker-password=${password} \
+    --docker-email=${email-address}
