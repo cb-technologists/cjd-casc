092963aa53455fc0e2879c63ae916a94ccc1fe46
Matt Elgin <melgin@cloudbees.com>
2019-07-03 15:42:03 -0400

    update secret creation and StatefulSet for gcr

--- create-secrets.sh
+++ create-secrets.sh
@@ -1,10 +1,14 @@
 kubectl create secret generic github --from-literal=username=${USERNAME} --from-literal=token=${TOKEN}
 
-kubectl create secret generic url --from-literal=url=${URL}
-
 kubectl create secret generic github-oauth --from-literal=CLIENT_ID=${CLIENT_ID} --from-literal=CLIENT_SECRET=${CLIENT_SECRET}
 
-kubectl create secret docker-registry docker-credentials \
-    --docker-username=${username}  \
-    --docker-password=${password} \
-    --docker-email=${email-address}
+# kubectl create secret docker-registry docker-credentials \
+#     --docker-username=${username}  \
+#     --docker-password=${password} \
+#     --docker-email=${email-address}
+
+kubectl create secret docker-registry gcr-secret \
+    --docker-server=https://gcr.io \
+    --docker-username=_json_key \
+    --docker-email=${SERVICE_ACCOUNT@PROJECT.iam.gserviceaccount.com} # replace with service account email \
+    --docker-password="$(cat kaniko-secret.json)"
