9b17192e7f87a7597d5b1a22773cf44fd2b261a6
Matt Elgin <melgin@cloudbees.com>
2019-09-05 15:42:46 -0400

    delete by string match

--- update-yaml-for-staging.sh
+++ update-yaml-for-staging.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 sed -i 's/cjd.cloudbees.elgin.io/cjd-staging.cloudbees.elgin.io/g' cjd.yaml
-sed -i '73,75d' cjd.yaml
-sed -i '77,80d' cjd.yaml
+sed -i '/cert-manager/d' cjd.yaml
 sed -i 's!https://cjd.cloudbees.elgin.io!http://cjd-staging.cloudbees.elgin.io!g' jenkinsCasc.yaml
